e00570323f8ee56904ef131300d517849352a3d2
franco moron <francomoron730@gmail.com>
2021-11-12 09:34:30 -0300

    Cambiando la imagen de inicio y creando el formato de nuestros productos

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const bar = document.getElementById("bar");
 const menu =document.getElementById("menu");
 const cross = document.getElementById("cross");
-
+const arrow = document.getElementById("arrow");
 bar.addEventListener("click", function(e){
     menu.classList.toggle("active")
 });
